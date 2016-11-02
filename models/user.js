096695a52cde5bea7893bf43d5eeedc4ee1a0502
wongpet5 <peterskwongut@gmail.com>
2016-11-01 23:41:36 -0400

    password hashing
    Used Bcrypt to Hash the password. A Hashed Password is now saved into
    Mongo.

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
 
 mongoose.connect('mongodb://localhost/nodeauth');
 
@@ -27,5 +28,14 @@ var UserSchema = mongoose.Schema({
 var User = module.exports = mongoose.model('User', UserSchema);
 
 module.exports.createUser = function(newUser, callback){
-    newUser.save(callback); 
+
+    // Hash Password before we save into Mongo
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(newUser.password, salt, function(err, hash) {
+            newUser.password = hash; 
+            newUser.save(callback);         
+        });
+    });
+
+    
 }
