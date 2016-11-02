1591a5c47f16ca5825bccf8282f60d9b97387ab8
wongpet5 <peterskwongut@gmail.com>
2016-11-01 22:53:17 -0400

    Form Validation
    Added Validation to Post: Users/Request. This used the ‘Express’
    Validator.

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var upload = multer({dest: './uploads'}); 
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
-/*router.get('/register', function(req, res, next) {
-  res.render('register');
-});*/
 
 router.get('/register', function(req, res, next) {
   res.render('register',{title:'Register'});
@@ -16,6 +14,50 @@ router.get('/register', function(req, res, next) {
 
 router.get('/login', function(req, res, next) {
   res.render('login',{title:'Login'});
+});
+
+router.post('/register', upload.single('profileimage'), function(req, res, next) {
+  console.log(req.body.name);
+  var name = req.body.name; 
+  var email = req.body.email; 
+  var username = req.body.username; 
+  var password = req.body.password; 
+  var password2 = req.body.password2; 
+
+  //console.log(req.file);
+  if(req.file)
+  {
+    console.log('Uploading File...');
+    var profileImage = req.file.filename; 
+  } 
+  else
+  {
+    console.log('No File Uploaded');
+    var profileImage = 'noimage.jpg'; // Standard Image we can show
+  }
+
+  // Form Validator
+  req.checkBody('name', 'Name field is required').notEmpty();
+  req.checkBody('email', 'Email field is required').notEmpty();
+  req.checkBody('email', 'Name field is required').isEmail();
+  req.checkBody('username', 'Username field is required').notEmpty();
+  req.checkBody('password', 'Password field is required').notEmpty();
+  req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+
+
+  // Check Errors 
+  var errors = req.validationErrors(); 
+
+  if (errors){
+    res.render('register', {
+      errors: errors
+    });
+  } else {
+    console.log('No Errors');
+  }
+
+
 });
 
 module.exports = router;
